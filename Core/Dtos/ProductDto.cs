b5751a9cd7d8c48718980d81cc0aa10dce98469d
Mizanur Rahman <mr.milonku@gmail.com>
2021-08-27 11:05:49 +0600

    Core Library Project Added

---
+++ Core/Dtos/ProductDto.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Dtos
+{
+    public class ProductDto
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal DeliveryPrice { get; set; }
+
+        [JsonIgnore]
+        public bool IsNew { get; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime LastModifiedOn { get; set; }
+    }
+}
---
+++ Core/Entities/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Entity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime LastModifiedOn { get; set; }
+    }
+}
