3fdc554ad3848dd8147892598a3beeac844c2188
Mizanur Rahman <mr.milonku@gmail.com>
2021-08-27 11:28:31 +0600

    Added Product Repository

---
+++ Core/Repositories/IProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Products> Get(Expression<Func<Products, bool>> predicate);
+
+        IEnumerable<Products> GetAll();
+
+        Products GetBy(long id);
+
+        Products Insert(Products model);
+
+        Products Update(Products model);
+
+        bool Delete(long id);
+    }
+}
---
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Core.Entities;
+using Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IRepository<Products> repository;
+
+        public ProductRepository(IRepository<Products> repository)
+        {
+            this.repository = repository;
+        }
+
+        public IEnumerable<Products> Get(Expression<Func<Products, bool>> predicate)
+        {
+            return repository.Get(predicate);
+        }
+
+        public IEnumerable<Products> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public Products GetBy(long id)
+        {
+            return repository.Get(id);
+        }
+
+        public Products Insert(Products model)
+        {
+            return repository.Insert(model);
+        }
+
+        public Products Update(Products model)
+        {
+            return repository.Update(model);
+        }
+
+        public bool Delete(long id)
+        {
+            repository.Delete(id);
+
+            return true;
+        }
+    }
+}
